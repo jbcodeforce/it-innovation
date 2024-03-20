2bd34e2d7232667e08a905bdf3ee10c34a0c78f8
jbcodeforce <jerome.boyer@gmail.com>
2024-03-20 13:44:38 -0700

    update to page content

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import json
 from PIL import Image
 from streamlit_lottie import st_lottie
+from st_pages import Page, show_pages
 
 # find emojis https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title='IT Innovation Consulting',page_icon="👥", layout="wide")
@@ -9,11 +10,23 @@ st.set_page_config(page_title='IT Innovation Consulting',page_icon="👥", layou
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
+
+with st.sidebar:
+    
+    show_pages(
+    [
+        Page("Main.py", "Home", "🏠"),
+        Page("pages/1_services.py", "Services",":question:"),
+        Page("pages/2_courses.py", "Courses", ":school:"),
+        Page("pages/3_blogs.py", "Blogs", ":book:"),
+        Page("pages/4_contact.py", "Contact", ":email:"),
+    ]
+)
     
 with st.container():
     st.title("IT Innovation Consulting")
     st.subheader("Welcome to Innovation")
-
+    st.markdown("Are you looking to transform your business and harness the full potential of the Cloud? Our expert consultants specialize in AWS Cloud architecture and application modernization, helping businesses like yours thrive in the digital era.")
    
 lottie_coding = load_lottiefile("images/an-1.json") 
 
@@ -22,15 +35,15 @@ with st.container():
     left_column, right_column = st.columns(2)
     with left_column:
         st.header("Architecture consultancy")
-        st.write("""
+        st.markdown("""
         Our clients benefit from our experience of delivering highly resilient and scalable systems in many different industries.
         We can guide you in adopting the correct strategies for successful development and choosing the right technologies for your business needs.
         Review your scalability needs, your requirements around high availability, disaster recovery, or data consistency.
         """)
         st.write("##")
     with right_column:
-        st.image("images/team-1.png",width=300)
-        st.write("Image created by StableDiffusionXL")
+        st.image("images/it-consultant-service.jpg",width=400)
+        # st.write("Image created by StableDiffusionXL")
 
 with st.container():
     st.write("---")
@@ -61,7 +74,9 @@ with st.container():
 
 with st.container():
     st.subheader("Event-driven")
-    left_column, right_column = st.columns((1,2))
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.image("images/eda-hl.png",width=380)
     with right_column:
         st.write("""
             Event-driven solution are for scaling and decreasing coupling between components. But once we design with events,
@@ -70,3 +85,14 @@ with st.container():
              with real-time analytics and complex event processing. Our unique expertize in rule engine, messaging and streaming middleware
              helps hundred of customers to be successful.
             """)
+        
+with st.container():
+    st.subheader("AI - Machine Learning - Generative AI")
+    left_column, right_column = st.columns((1,1))
+    with left_column:
+        st.write("""
+            
+            """)
+    with right_column:
+        st.image("images/llm-ra-1.png",width=400)
+        
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 # A streamlit web app
+
+## Run locally
+
+```sh
+pip install -r requirements.txt
+streamlit run main.py
+```
+
+
